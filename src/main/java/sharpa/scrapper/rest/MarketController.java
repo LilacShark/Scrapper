15069517f1b7ca1e000e761e156831e92b051c7c
TrickyObject <markertosummer@gmail.com>
2023-05-13 18:21:40 +0700

    Рефактор на спринг бут + Дата Добавил шедьюлер для запросов

--- src/main/java/sharpa/scrapper/rest/MarketController.java
+++ src/main/java/sharpa/scrapper/rest/MarketController.java
@@ -1,53 +1,30 @@
 package sharpa.scrapper.rest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import sharpa.scrapper.business.MarketManager;
 import sharpa.scrapper.view.MarketRequest;
 import sharpa.scrapper.view.MarketResponse;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-//@Service("controller")
-//@Path("/in")
-//@RequestMapping("/rest/userAccount")
-//@Path("/price")
-//@Component
-@Service("controller")
-@Path("/in")
+@RestController
+@RequestMapping("api")
 public class MarketController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MarketController.class);
 
     @Autowired
     @Qualifier("marketService")
     private MarketManager marketManager;
 
-//    @Path("/price")
-//    @Consumes(MediaType.APPLICATION_JSON)
+    @RequestMapping(value = "/prices", method = RequestMethod.POST, consumes="application/json", produces = "application/json")
+    public MarketResponse showMarketPrice(@RequestBody MarketRequest marketRequest) {
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public MarketResponse showMarketPrice(MarketRequest marketRequest) {
-
-        logger.info("showMarketPrice.. ");
-
+        System.out.println("showMarketPrice..");
         return marketManager.showPrice(marketRequest.getGoodName());
 
     }
-
-
 
 }
