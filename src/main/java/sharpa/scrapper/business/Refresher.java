3c00d4559cb1e7234c7a71931080b7938048d534
TrickyObject <markertosummer@gmail.com>
2023-05-15 03:12:23 +0700

    Добавлен сторэдж для хранения операций и того, что имеем в инвентаре

--- src/main/java/sharpa/scrapper/business/Refresher.java
+++ src/main/java/sharpa/scrapper/business/Refresher.java
@@ -1,4 +1,4 @@
-package sharpa.scrapper;
+package sharpa.scrapper.business;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import sharpa.scrapper.business.MarketManager;
 import sharpa.scrapper.model.Goods;
 import sharpa.scrapper.view.MarketGetServiceResponse;
 import sharpa.scrapper.web.GetService;
---
+++ src/main/java/sharpa/scrapper/model/Storage.java
@@ -0,0 +1,10 @@
+package sharpa.scrapper.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Storage {
+
+
+
+}
